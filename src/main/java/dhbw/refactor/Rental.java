01eb6d0190fefa18e6dfa1df7dd0e1183e2f53e9
rbnsch <robinschmidt0@gmx.de>
2021-05-09 17:07:50 +0200

    Deleted unnecessary Function calls

--- src/main/java/dhbw/refactor/Rental.java
+++ src/main/java/dhbw/refactor/Rental.java
@@ -5,9 +5,9 @@ import static dhbw.refactor.PriceCode.NEW_RELEASE;
 public class Rental {
     private final Movie movie;
     private final int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
     public int getDaysRented() {
         return daysRented;
@@ -18,17 +18,17 @@ public class Rental {
 
     public double getCharge() {
         double result = 0;
-        switch (getMovie().getPriceCode()) {
+        switch (movie.getPriceCode()) {
             case REGULAR -> {
                 result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
             }
-            case NEW_RELEASE -> result += getDaysRented() * 3;
+            case NEW_RELEASE -> result += daysRented * 3;
             case CHILDREN -> {
                 result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
             }
             default -> System.out.println("Error!");
 
@@ -37,7 +37,7 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == NEW_RELEASE) && getDaysRented() > 1) {
+        if ((movie.getPriceCode() == NEW_RELEASE) && daysRented > 1) {
             return 2;
         }
         return 1;
