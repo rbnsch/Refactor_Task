2a06d98cac40e70efb9bb648709dade692acc5e4
rbnsch <robinschmidt0@gmx.de>
2021-05-09 15:38:12 +0200

    Removed unused imports

--- src/main/java/dhbw/refactor/Program.java
+++ src/main/java/dhbw/refactor/Program.java
@@ -2,7 +2,7 @@ package dhbw.refactor;
 
 
 import java.lang.*;
-import java.util.*;
+
 /**
  * Note that Java console applications need to be run through the java runtime
  * by running "java -jar JarFile.jar" in the command line.
