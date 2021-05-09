1b0ffeaf308f37b9cf0310f5631f26848e3664fc
rbnsch <robinschmidt0@gmx.de>
2021-05-09 16:05:13 +0200

    Replaced Switch with enhanced Switch Case

--- src/main/java/dhbw/refactor/Customer.java
+++ src/main/java/dhbw/refactor/Customer.java
@@ -46,19 +46,18 @@ public class Customer {
     private double amountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
-            case REGULAR:
+            case REGULAR -> {
                 thisAmount += 2;
                 if (each.getDaysRented() > 2)
                     thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case CHILDREN:
+            }
+            case NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
+            case CHILDREN -> {
                 thisAmount += 1.5;
                 if (each.getDaysRented() > 3)
                     thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
+            }
+
         }
         return thisAmount;
     }
